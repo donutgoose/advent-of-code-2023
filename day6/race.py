a5ad1c9dc87b9e1c947b80d9e017b02630ce71bd
Daniel L <64748981+donutgoose@users.noreply.github.com>
2023-12-06 15:12:04 +0000

    minor optimizations (rewrote the whole thing)

--- day6/race.py
+++ day6/race.py
@@ -1,40 +1,8 @@
 import math
-
 file = open('data', 'r').read().split('\n')
-
-times = list(map(int, list(filter(lambda x: x != '', file[0].split(' ')))[1:]))
-distances = list(map(int, list(filter(lambda x: x != '', file[1].split(' ')))[1:]))
-
-acc = ''
-for t in times:
-    acc += str(t)
-times = [int(acc)]
-
-acc = ''
-for d in distances:
-    acc += str(d)
-distances = [int(acc)]
-
-print(times)
-print(distances)
-
-def halfOrSomething(n):
-    return math.floor(n/4)
-
-acc = 0
-vals = []
-for index, t in enumerate(times):
-    record = distances[index]
-    for i in range(t):
-        
-        speed = i
-        time = t - i
-        if (speed * time) > record:
-            acc += 1
-    vals.append(acc)
-    acc = 0
-
+times = [int(''.join(list(filter(lambda x: x != '', file[0].split(' ')))[1:]))]
+distances = [int(''.join(list(filter(lambda x: x != '', file[1].split(' ')))[1:]))]
 acc = 1
-for val in vals:
-    acc *= val
+for index, t in enumerate(times):
+    acc *= (math.floor(((-1*t)-math.sqrt((t*t)-(4*distances[index])))/-2)-math.floor((((-1*t)+math.sqrt((t*t)-(4*distances[index])))/-2)))
 print(acc)
